112fd63908ad3574550c240e20f33904d1aea8e6
Pietrell <46609827+Pietrell@users.noreply.github.com>
2020-02-02 14:16:16 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: MainActivity(),
+      home:  MainActivity(),
     );
   }
 }
